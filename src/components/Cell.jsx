dceb74d3e1a63e8607947a00f115613cb3778480
Pravin <prav.ch5@gmail.com>
2022-06-24 22:13:40 +0530

    placed Quuens

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './css/Cell.css'
+import {GiQueenCrown} from 'react-icons/gi'
 
-const Cell = ({i,j}) => {
+const Cell = ({i,j,c}) => {
   return (
-    <div style={{background: (i+j)%2===0?'white':'black'}} className='cell'></div>
+    <div style={{background: (i+j)%2===0?'white':'black', color: (i+j)%2===1?'white':'black'}} className='cell'>
+      {c && <h1 className='queen-cell'><GiQueenCrown/></h1>}
+    </div>
   )
 }
 
